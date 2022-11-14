38975effcf8c5a747050577174239707bfb50e7a
XDTerminated <gupta.sayam2006@gmail.com>
2022-11-14 14:08:40 +0000

    finished player class

--- phrases/Player.java
+++ phrases/Player.java
@@ -1,3 +1,28 @@
 public class Player {
-    
+    private String playerName;
+    private int playerScore;
+
+    Player (String playerName, int playerScore) {
+        this.playerName = playerName;
+        this.playerScore = playerScore;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
+    }
+
+
+
 }
