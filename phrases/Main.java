5490b1261be9acca1b7b2a592c033006e23cf29c
XDTerminated <gupta.sayam2006@gmail.com>
2022-11-14 14:30:38 +0000

    finished project

--- phrases/Main.java
+++ phrases/Main.java
@@ -9,15 +9,40 @@ public class Main {
         String phrase = board.getPhrase();
 
         String lines = board.getLines(phrase);
+
+        System.out.println("Player 1 Name: ");
+        String player1name = scanner.next();
+        System.out.println("Player 2 Name: ");
+        String player2name = scanner.next();
+
+        Player player1 = new Player(player1name, 0);
+        Player player2 = new Player(player2name, 0);
+
         
-        int tries = 10;
+        int playerTries1 = 5;
+        int playerTries2 = 5;
+
+        int turn = 0;
+
         while (true) {
-            System.out.println("Tries Left: " + tries);
+
+            if (turn == 0) {
+                System.out.println("Player 1 Turn: ");
+                System.out.println("Tries Left: " + playerTries1);
+            } else {
+                System.out.println("Player 2 Turn: ");
+                System.out.println("Tries Left: " + playerTries1);
+            }
+
             System.out.println(lines);
             String character = scanner.next();
             
             if (!phrase.contains(character)) {
-                tries--;
+                if (turn == 0) {
+                    playerTries1--;
+                } else {
+                    playerTries2--;
+                }
             }
 
             while (phrase.contains(character)) {
@@ -29,13 +54,23 @@ public class Main {
             + lines.substring(index + 1, lines.length());
             }
 
-            if (tries == 0) {
-                System.out.println("You lost, better luck next time");
+            if (playerTries1 == 0) {
+                System.out.println(player1.getPlayerName() + " loses, " + player2.getPlayerName() + " wins.");
                 break;
-            } else if (!lines.contains("-")) {
-                System.out.println("Congratulations, you win.");
+            } else if (playerTries2 == 0) {
+                System.out.println(player2.getPlayerName() + " loses, " + player1.getPlayerName() + " wins.");
+                break;
+            } else if (!lines.contains("-") && turn == 0) {
+                System.out.println("Congratulations"  + player1.getPlayerName() + " win.");
                 break;
             }
+
+            if (turn == 0) {
+                turn = 1;
+            } else {
+                turn = 0;
+            }
+
 
         }
 
